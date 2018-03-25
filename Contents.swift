a2cd01a01b9f7aada5bdf07fe913cfb51c76cae8
MAC <mac@macde-macbook-pro-4.local>
2018-03-25 14:22:53 +0800

    閏年計算

---
+++ Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func getData(date:Int) -> Bool{
+    if date % 4 == 0{
+        return true
+        
+    }
+    else if date % 100 == 0{
+        if date % 400 == 0{
+            return true
+        }
+    }
+    return false
+}
+var result = getData(date:1992)
